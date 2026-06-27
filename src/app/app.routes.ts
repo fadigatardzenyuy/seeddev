48bbd5afe8ff6ca925f542a6f645dc627d964674
ELUMBA-maker <elumba237@gmail.com>
2026-06-27 12:50:34 +0100

    Project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Navbar } from './components/navbar/navbar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+
+ { path: '', component: Navbar }
+];
