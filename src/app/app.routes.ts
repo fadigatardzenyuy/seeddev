41f8b9418aa54304b0b4e929bb4d109ab5a50b5d
ELUMBA-maker <elumba237@gmail.com>
2026-07-07 21:14:51 +0100

    added contact page for support and advrese inquiries

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './components/homeComponent/homeComponent';
 import { ProductsComponent } from './components/pricing/products/products';
 import { Pricing } from './components/pricing/pricing';
 import { WhatsNew } from './components/whats-new/whats-new';
+import { Contact } from './components/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'pricing', component: Pricing },
-  { path: 'whats-new', component: WhatsNew }
+  { path: 'whats-new', component: WhatsNew },
+  { path: 'contact-us', component: Contact },
+  
 ];
